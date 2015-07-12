ac8c2ce2289e1a2804533efa1015aa2d9da95caa
iGrog <igrog@kyra.fi>
2015-07-12 12:12:53 +0300

    Initial release

--- models/CoordHelper.php
+++ models/CoordHelper.php
@@ -18,31 +18,60 @@ class CoordHelper
         $distY = $origHeight - $rightDownY;
         $distX = $origWidth - $rightDownX;
 
-        if($distX < $distY) // Ограничитель у нас ось Y
+        // Попробуем переместить левую-верхнюю точку в X=0
+        $test1 = $leftUpY - ($distX / $koeff);
+        // Пробуем переместить левую верхнюю точку в Y=0
+        $test2 = $leftUpX - ($koeff * $distY);
+
+        // Переносим X в 0, Y остается в пределах картинки
+        if($test1 >= 0 && $test2 < 0)
         {
             $leftUpX = 0;
             $rightDownX = $origWidth;
-            $deltaY = $distX / $koeff;
-            $leftUpY -= $deltaY;
-            $rightDownY += $deltaY;
+            $leftUpY = $test1;
+            $rightDownY = $origHeight - $test1;
         }
-        else if($distY < $distX) // Ограничитель у нас ось X
+        else if($test2 >= 0 && $test1 < 0) // Переносим Y в 0, X - остается в пределах картинки
         {
             $leftUpY = 0;
+            $leftUpX = $test2;
             $rightDownY = $origHeight;
-            $deltaX = $koeff * $distY;
-            $leftUpX -= $deltaX;
-            $rightDownX += $deltaX;
+            $rightDownX = $origWidth - $test2;
         }
-        else
+        else // квадрат надо вписать в квадрат,
         {
-
+            $leftUpX = 0;
+            $leftUpY = 0;
+            $rightDownX = $origWidth;
+            $rightDownY = $origHeight;
         }
 
-        if($leftUpY < 0) $leftUpY = 0;
-        if($leftUpX < 0) $leftUpX = 0;
-        if($rightDownX > $origWidth) $rightDownX = $origWidth;
-        if($rightDownY > $origHeight) $rightDownY = $origHeight;
+
+//        if($distX < $distY) // Ограничитель у нас ось Y
+//        {
+//            $leftUpX = 0;
+//            $rightDownX = $origWidth;
+//            $deltaY = $distX / $koeff;
+//            $leftUpY -= $deltaY;
+//            $rightDownY += $deltaY;
+//        }
+//        else if($distY < $distX) // Ограничитель у нас ось X
+//        {
+//            $leftUpY = 0;
+//            $rightDownY = $origHeight;
+//            $deltaX = $koeff * $distY;
+//            $leftUpX -= $deltaX;
+//            $rightDownX += $deltaX;
+//        }
+//        else
+//        {
+//
+//        }
+//
+//        if($leftUpY < 0) $leftUpY = 0;
+//        if($leftUpX < 0) $leftUpX = 0;
+//        if($rightDownX > $origWidth) $rightDownX = $origWidth;
+//        if($rightDownY > $origHeight) $rightDownY = $origHeight;
 
 
         $wNew = $rightDownX - $leftUpX;
